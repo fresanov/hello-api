0481450f4b752592dd53311c25adc08f855a1fdc
fresanov <filip.resanovic@yahoo.com>
2023-11-25 15:35:59 +0100

    Add aws lambda dependenices Add aws lambda deploy step to pipeline

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
-	"log"
+	"context"
 	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 	"github.com/fresanov/hello-api/handlers/rest"
 )
 
+var httpLambda *httpadapter.HandlerAdapter
+
+func init() {
+	http.HandleFunc("/hello", rest.TranslateHandler)
+
+	httpLambda = httpadapter.New(http.DefaultServeMux)
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	return httpLambda.ProxyWithContext(ctx, req)
+}
+
 func main() {
 
-	addr := ":8080"
+	/* addr := ":8080"
 
 	mux := http.NewServeMux()
 
@@ -17,7 +33,9 @@ func main() {
 
 	log.Printf("listening on %s\n", addr)
 
-	log.Fatal(http.ListenAndServe(addr, mux))
+	log.Fatal(http.ListenAndServe(addr, mux)) */
+
+	lambda.Start(Handler)
 }
 
 type Resp struct {
