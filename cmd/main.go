0393b7239abee8cca305b0dbdca28e7fb8588aa7
fresanov <filip.resanovic@yahoo.com>
2023-11-26 15:04:03 +0100

    Add health endpoint Remove commented out and redunant code

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
+	"github.com/fresanov/hello-api/handlers"
 	"github.com/fresanov/hello-api/handlers/rest"
 	"github.com/gorilla/mux"
 )
 
 func main() {
-
-	/* addr := ":8080"
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/hello", rest.TranslateHandler)
-
-	log.Printf("listening on %s\n", addr)
-
-	log.Fatal(http.ListenAndServe(addr, mux)) */
-
 	router := mux.NewRouter()
-	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Not found", r.RequestURI)
-		http.Error(w, fmt.Sprintf("Not found: %s", r.RequestURI), http.StatusNotFound)
-	})
 	router.HandleFunc("/hello", rest.TranslateHandler).Methods("GET")
+	router.HandleFunc("/health", handlers.HealthCheck).Methods("GET")
+
 	adapter := gorillamux.NewV2(router)
 
 	lambda.Start(adapter.ProxyWithContext)
